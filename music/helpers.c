2a7e4512692a6e3167a8a06ff7a79052c67a0575
Matthew Rizzini <matthewrizzini@gmail.com>
2018-04-12 20:16:14 +0000

    is_rest, birthday, and duration working

--- music/helpers.c
+++ music/helpers.c
@@ -2,12 +2,50 @@
 
 #include <cs50.h>
 #include <string.h>
+#include <stdlib.h>
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
     // TODO
+    // take a fraction as a STRING (1/8, 1/4,/ 3/8)
+    // assume numerator and denominator are each one digit
+    // assume the fraction works out to a whole number of eighth notes
+    // Returns the number (INTEGER) of eigth notes long (1/8 = 1 | 1/4 = 2 | 3/8 = 3)
+    // possible denominator values are 8, 4, 2, 1
+    // int numerator = atoi(fraction[0]);
+    // int denominator = atoi(fraction[2]);
+
+    char num = fraction[0];
+    char den = fraction[2];
+    float numerator = num - '0';
+    float denominator = den - '0';
+    float division = numerator / denominator;
+    int noteLength;
+
+        if (division == .125)
+    {
+        noteLength = 1;
+        return noteLength;
+    }
+    if (division == .25)
+    {
+        noteLength = 2;
+        return noteLength;
+    }
+    if (division == .375)
+    {
+        noteLength = 3;
+        return noteLength;
+    }
+    if (division == .5)
+    {
+        noteLength = 4;
+        return noteLength;
+    }
+
+
 }
 
 // Calculates frequency (in Hz) of a note
